67b569428ae3371a4c1754176a2630b35403fc1b
Pierre-Yves Ritschard <pyr@spootnik.org>
2019-11-28 06:56:04 +0100

    project: add release repositories

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject spootnik/aleph-params "0.1.0-SNAPSHOT"
   :description "Netty-based query string decoder"
-  :url "https://github.com/pyr/fast-params"
+  :url "https://github.com/pyr/aleph-params"
   :license {:name "MIT/ISC"}
   :dependencies [[org.clojure/clojure  "1.10.1"]
                  [io.netty/netty-all    "4.1.43.Final"]]
-  :profiles {:dev {:dependencies [[criterium            "0.4.5"]
-                                  [exoscale/interceptor "0.1.6"]
+  :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
+  :profiles {:dev {:dependencies [[exoscale/interceptor "0.1.6"]
                                   [aleph                "0.4.7-alpha5"]]}})

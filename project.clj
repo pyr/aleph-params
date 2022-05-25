d0857a9d6be39cfcdb0e1762d6a90c25ee65df95
Pierre-Yves Ritschard <pyr@spootnik.org>
2022-05-25 08:17:11 +0200

    Version 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/aleph-params "0.1.4-SNAPSHOT"
+(defproject spootnik/aleph-params "0.1.4"
   :description "Netty-based query string decoder"
   :url "https://github.com/pyr/aleph-params"
   :license {:name "MIT/ISC"}

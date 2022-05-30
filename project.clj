f5bfe4b410dda7ac1a45dda2f6852ed8d3a25818
Pierre-Yves Ritschard <pyr@spootnik.org>
2022-05-30 15:30:11 +0200

    project: correct deps

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/pyr/aleph-params"
   :license {:name "MIT/ISC"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [io.netty/netty-codec "4.1.77.Final"]]
+                 [io.netty/netty-codec-http "4.1.77.Final"]]
   :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
   :pedantic? :abort
   :global-vars {*warn-on-reflection* true}
-  :profiles {:dev {:dependencies [[exoscale/interceptor "0.1.10"]
-                                  [aleph                "0.4.7"]]
-                   :pedantic?    :ignore
-                   :global-vars  {*warn-on-reflection* false}}})
+  :profiles {:test {:dependencies [[exoscale/interceptor "0.1.10"]
+                                   [aleph                "0.4.7"]]
+                    :pedantic?    :ignore
+                    :global-vars  {*warn-on-reflection* false}}})
